e0dbc67e28e7cde0edd034c78b5bd87a5328e5c8
Sebas <joan.saavedra@correounivalle.edu.co>
2025-03-19 22:12:35 -0500

    feat:creating product and category entities

--- src/main/java/com/ds3/team8/products_service/entities/Category.java
+++ src/main/java/com/ds3/team8/products_service/entities/Category.java
@@ -1,4 +1,32 @@
 package com.ds3.team8.products_service.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer categoryId;
+
+    @NotEmpty(message = "No puede estar vacio")
+    @Size(min = 2, max = 20, message = "El tamaño tiene que estar entre 2 y 20")
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Product> products;
 }
--- src/main/java/com/ds3/team8/products_service/entities/Product.java
+++ src/main/java/com/ds3/team8/products_service/entities/Product.java
@@ -1,4 +1,49 @@
 package com.ds3.team8.products_service.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer productId;
+
+    @Size(min = 2, max = 20, message = "El tamaño tiene que estar entre 2 y 20")
+    @Column(nullable = false)
+    private String name;
+
+    @Size(max = 255, message = "La descripción no puede tener más de 255 caracteres")
+    private String description;
+
+    @NotNull(message = "El precio es obligatorio")
+    @Min(value = 0, message = "El precio no puede ser negativo")
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @NotNull(message = "El stock no puede ser nulo")
+    @Min(value = 0, message = "El stock no puede ser negativo")
+    @Column(nullable = false)
+    private Integer stock;
+
+    // Relación Many-to-One con Category
+    // producto a categoria---- categoria muchos productos
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "categoryId", nullable = false)
+    private Category category;
 }
