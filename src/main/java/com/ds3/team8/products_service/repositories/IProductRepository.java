28ca2efef7978fe56485ff6ba9cef1c7095830c5
Victor Alzate <victoralzate61@gmail.com>
2025-06-14 20:17:50 -0500

    feat: refactorizando el codigo para mejorar calidad de codigo

--- src/main/java/com/ds3/team8/products_service/repositories/IProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ds3.team8.products_service.repositories;
-
-import com.ds3.team8.products_service.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IProductRepository extends JpaRepository<Product, Long> {
-}
--- src/main/java/com/ds3/team8/products_service/services/IProductService.java
+++
@@ -1,9 +0,0 @@
-package com.ds3.team8.products_service.services;
-
-import com.ds3.team8.products_service.dtos.ProductRequest;
-import com.ds3.team8.products_service.dtos.ProductResponse;
-import com.ds3.team8.products_service.entities.Product;
-
-public interface IProductService {
-    ProductResponse save(ProductRequest productRequest);
-}
