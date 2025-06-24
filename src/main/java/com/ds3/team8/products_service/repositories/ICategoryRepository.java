fc7686b0ac1958c28cb54515ed0fc371dca2de11
Sebas <joan.saavedra@correounivalle.edu.co>
2025-06-23 22:29:36 -0500

    feat:adjusting repositories
    Se ajusta el codigo de repositorio.

--- src/main/java/com/ds3/team8/products_service/repositories/ICategoryRepository.java
+++ src/main/java/com/ds3/team8/products_service/repositories/ICategoryRepository.java
@@ -3,13 +3,16 @@ package com.ds3.team8.products_service.repositories;
 import com.ds3.team8.products_service.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ICategoryRepository extends JpaRepository<Category, Long> {
-    Optional<Category> findByName(String name);
-    // metodo para encontrar activas
-    List<Category> findByIsActiveTrue();
+    Optional<Category> findByNameAndIsActiveTrue(String name); // Obtener categoría por nombre y activo
+    Optional<Category> findByIdAndIsActiveTrue(Long id); // Obtener categoría por ID y activo
+    List<Category> findAllByIsActiveTrue(); // Obtener todas las categorías activas
+    Page<Category> findAllByIsActiveTrue(Pageable pageable); // Obtener todas las categorías activas con paginación
 }
--- src/main/java/com/ds3/team8/products_service/repositories/IProductRepository.java
+++ src/main/java/com/ds3/team8/products_service/repositories/IProductRepository.java
@@ -3,7 +3,17 @@ package com.ds3.team8.products_service.repositories;
 import com.ds3.team8.products_service.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface IProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByIdAndIsActiveTrue(Long id); // Obtener producto por ID y activo
+    List<Product> findAllByIsActiveTrue(); // Obtener todos los productos activos
+    Page<Product> findAllByIsActiveTrue(Pageable pageable); // Obtener todos los productos activos con paginación
+    List<Product> findByCategoryIdAndIsActiveTrue(Long categoryId); // Obtener productos por ID de categoría y activo
+    Page<Product> findByCategoryIdAndIsActiveTrue(Long categoryId, Pageable pageable); // Obtener productos por ID de categoría y activo con paginación
 }
